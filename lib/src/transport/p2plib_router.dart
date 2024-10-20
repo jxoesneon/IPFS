30f4152f314b7882e974c419adfe075363f16e2a
Eduardo Rojas <joseeduardox@gmail.com>
2024-10-20 01:29:46 -0600

    Implement a P2P lib router using `p2plib` package
    - Added a new class `P2plibRouter` implementing router functionality with `p2plib` package.
    - Features include creating/configuring router, starting/stopping, sending/receiving messages, resolving peer IDs.
    - Initial configuration with bootstrap peers implemented.

---
+++ lib/src/transport/p2plib_router.dart
@@ -0,0 +1,74 @@
+// lib/src/transport/p2plib_router.dart
+
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:ipfs/src/core/config/config.dart';
+import 'package:ipfs/src/utils/base58.dart';
+import 'package:p2plib/p2plib.dart' as p2p;
+
+/// A router implementation using the `p2plib` package.
+class P2plibRouter {
+  /// Creates a new [P2plibRouter] with the given [config].
+  P2plibRouter(this._config)
+      : _router = p2p.RouterL0(crypto: p2p.Crypto(p2p.Ed25519())) {
+    // Initialize the router with the provided configuration
+    _router.peerId = p2p.PeerId(value: _generateRandomPeerId());
+    _router.transports = [p2p.TransportUdp(ip4: true, ip6: true)];
+    _router.keepalivePeriod = const Duration(seconds: 30);
+    _router.messageTTL = const Duration(minutes: 1);
+  }
+
+  final IPFSConfig _config;
+  final p2p.RouterL0 _router;
+
+  /// The peer ID of this node.
+  String get peerID => _router.peerId.toBase58String();
+
+  /// The connected peers.
+  List<p2p.Peer> get connectedPeers =>
+      _router.routes.values.map((e) => e.peer).toList();
+
+  /// Starts the router.
+  Future<void> start() async {
+    await _router.start();
+    // Connect to bootstrap peers
+    for (final peer in _config.bootstrapPeers) {
+      try {
+        await _router.addPeer(p2p.PeerId.fromBase58String(peer));
+      } catch (e) {
+        print('Error adding bootstrap peer: $e');
+      }
+    }
+  }
+
+  /// Stops the router.
+  Future<void> stop() async {
+    await _router.stop();
+  }
+
+  /// Sends a message to a peer.
+  Future<void> sendMessage(p2p.Peer peer, Uint8List message) async {
+    await _router.sendDatagram(
+      addresses: [peer.address.ip],
+      datagram: message,
+    );
+  }
+
+  /// Receives a message from a peer.
+  Future<Uint8List> receiveMessage(p2p.Peer peer) async {
+    // TODO: Implement logic to receive messages with a timeout
+    throw UnimplementedError();
+  }
+
+  /// Resolves a peer ID to a list of addresses.
+  List<String> resolvePeerId(p2p.PeerId peerId) {
+    return _router.resolvePeerId(peerId);
+  }
+
+  // Generates a random peer ID
+  Uint8List _generateRandomPeerId() {
+    final random = Random.secure();
+    return Uint8List.fromList(List.generate(32, (i) => random.nextInt(256)));
+  }
+}
