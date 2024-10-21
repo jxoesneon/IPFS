3915c922d9d82eb35e8df44dcf53354395281c78
joseeduardox <joseeduardox@gmail.com>
2024-10-21 06:01:29 +0000

    Refactor deletion logic in RedBlackTree class
    - Refactored deletion method in the RedBlackTree class to improve readability and maintainability
    - Updated method signatures and removed unnecessary imports and code redundancies
    - Introduced a FixViolations instance to handle violations after deletion

--- lib/src/protocols/dht/red_black_tree/deletion.dart
+++ lib/src/protocols/dht/red_black_tree/deletion.dart
@@ -1,19 +1,14 @@
 // lib/src/protocols/dht/red_black_tree/deletion.dart
 import '../red_black_tree.dart';
-import 'fix_violations.dart' as fix_violations;
-import 'rotations.dart' as rotations;
+import 'fix_violations.dart';
 import '/../src/proto/dht/common_tree.pb.dart' as common_tree;
-import 'search.dart';
-
 
 class Deletion<K_PeerId, V_PeerInfo> {
   void delete(RedBlackTree<K_PeerId, V_PeerInfo> tree, K_PeerId key) {
     deleteNode(tree, key);
-
   }
 
-  void deleteNode<K_PeerId, V_PeerInfo>(
-      RedBlackTree<K_PeerId, V_PeerInfo> tree, K_PeerId key) {
+  void deleteNode(RedBlackTree<K_PeerId, V_PeerInfo> tree, K_PeerId key) {
     RedBlackTreeNode<K_PeerId, V_PeerInfo>? z = searchNode(tree, key);
 
     if (z == null) {
@@ -32,11 +27,11 @@ class Deletion<K_PeerId, V_PeerInfo> {
       x = z.left_child;
       transplant(tree, z, z.left_child);
     } else {
-      y = _minimum(z.right_child!);
+      y = minimum(z.right_child!);
       yOriginalColor = y!.color;
       x = y.right_child;
       if (y.parent == z) {
-        x?.parent = y; // if x is null, it's the root node and should have a null parent.
+        x?.parent = y;
       } else {
         transplant(tree, y, y.right_child);
         y.right_child = z.right_child;
@@ -44,30 +39,33 @@ class Deletion<K_PeerId, V_PeerInfo> {
       }
       transplant(tree, z, y);
       y.left_child = z.left_child;
-      y.left_child?.parent = y
-  ;
+      y.left_child?.parent = y;
       y.color = z.color;
     }
 
+    // Create an instance of FixViolations to fix any violations after deletion
+    FixViolations<K_PeerId, V_PeerInfo> fixViolations =
+        FixViolations<K_PeerId, V_PeerInfo>();
+
     if (yOriginalColor == common_tree.NodeColor.BLACK) {
-      fix_violations.fixDeletion(tree, x, x?.parent); // Passing x and its parent to fixDeletion
+      fixViolations.fixDeletion(tree, x!, x.parent); // Fix any violations
     }
   }
 
-  void transplant<K_PeerId, V_PeerInfo>(RedBlackTree<K_PeerId, V_PeerInfo> tree,
+  void transplant(RedBlackTree<K_PeerId, V_PeerInfo> tree,
       RedBlackTreeNode<K_PeerId, V_PeerInfo> u,
       RedBlackTreeNode<K_PeerId, V_PeerInfo>? v) {
     if (u.parent == null) {
-      tree._root = v;
-    } else if (u == u.parent.left_child) {
-      u.parent.left_child = v;
+      tree.root = v;
+    } else if (u == u.parent!.left_child) {
+      u.parent!.left_child = v;
     } else {
-      u.parent.right_child = v;
+      u.parent!.right_child = v;
     }
     v?.parent = u.parent;
   }
 
-  RedBlackTreeNode<K_PeerId, V_PeerInfo>? _minimum<K_PeerId, V_PeerInfo>(
+  RedBlackTreeNode<K_PeerId, V_PeerInfo>? minimum(
       RedBlackTreeNode<K_PeerId, V_PeerInfo> node) {
     while (node.left_child != null) {
       node = node.left_child!;
@@ -75,19 +73,19 @@ class Deletion<K_PeerId, V_PeerInfo> {
     return node;
   }
 
-  RedBlackTreeNode<K_PeerId, V_PeerInfo>? searchNode<K_PeerId, V_PeerInfo>(
+  RedBlackTreeNode<K_PeerId, V_PeerInfo>? searchNode(
       RedBlackTree<K_PeerId, V_PeerInfo> tree, K_PeerId key) {
     RedBlackTreeNode<K_PeerId, V_PeerInfo>? node = tree.root;
 
     while (node != null) {
-      final comparison = tree.compare(key, node.key);
+      final comparison = tree.compare(key, node.key as K_PeerId);
 
       if (comparison == 0) {
         return node; // Key found
       } else if (comparison < 0) {
-        node = node.left_child; // Search in the left subtree
+        node = node.left_child; // Search in the left_child subtree
       } else {
-        node = node.right_child; // Search in the right subtree
+        node = node.right_child; // Search in the right_child subtree
       }
     }
 
