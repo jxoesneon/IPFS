cb170ed20fd6d427ddcdf793c2d879b973917667
joseeduardox <joseeduardox@gmail.com>
2024-10-21 03:33:45 +0000

    Refactor RedBlackTreeNode field names in red_black_tree.pbjson.dart
    - Update field names in RedBlackTreeNode from 'K' to 'key' and 'V' to 'value'.
    - Rename 'left_child' to 'left' and 'right_child' to 'right' for better readability.

--- lib/src/proto/dht/red_black_tree.pbjson.dart
+++ lib/src/proto/dht/red_black_tree.pbjson.dart
@@ -17,19 +17,20 @@ import 'dart:typed_data' as $typed_data;
 const RedBlackTreeNode$json = {
   '1': 'RedBlackTreeNode',
   '2': [
-    {'1': 'K', '3': 1, '4': 1, '5': 5, '10': 'K'},
-    {'1': 'V', '3': 2, '4': 1, '5': 11, '6': '.ipfs.dht.common.Node', '10': 'V'},
+    {'1': 'key', '3': 1, '4': 1, '5': 11, '6': '.ipfs.dht.common.K_PeerId', '10': 'key'},
+    {'1': 'value', '3': 2, '4': 1, '5': 11, '6': '.ipfs.dht.common.V_PeerInfo', '10': 'value'},
     {'1': 'color', '3': 3, '4': 1, '5': 14, '6': '.ipfs.dht.common.NodeColor', '10': 'color'},
-    {'1': 'left_child', '3': 4, '4': 1, '5': 11, '6': '.ipfs.dht.red_black_tree.RedBlackTreeNode', '10': 'leftChild'},
-    {'1': 'right_child', '3': 5, '4': 1, '5': 11, '6': '.ipfs.dht.red_black_tree.RedBlackTreeNode', '10': 'rightChild'},
+    {'1': 'left', '3': 4, '4': 1, '5': 11, '6': '.ipfs.dht.red_black_tree.RedBlackTreeNode', '10': 'left'},
+    {'1': 'right', '3': 5, '4': 1, '5': 11, '6': '.ipfs.dht.red_black_tree.RedBlackTreeNode', '10': 'right'},
   ],
 };
 
 /// Descriptor for `RedBlackTreeNode`. Decode as a `google.protobuf.DescriptorProto`.
 final $typed_data.Uint8List redBlackTreeNodeDescriptor = $convert.base64Decode(
-    'ChBSZWRCbGFja1RyZWVOb2RlEgwKAUsYASABKAVSAUsSIwoBVhgCIAEoCzIVLmlwZnMuZGh0Lm'
-    'NvbW1vbi5Ob2RlUgFWEjAKBWNvbG9yGAMgASgOMhouaXBmcy5kaHQuY29tbW9uLk5vZGVDb2xv'
-    'clIFY29sb3ISSAoKbGVmdF9jaGlsZBgEIAEoCzIpLmlwZnMuZGh0LnJlZF9ibGFja190cmVlLl'
-    'JlZEJsYWNrVHJlZU5vZGVSCWxlZnRDaGlsZBJKCgtyaWdodF9jaGlsZBgFIAEoCzIpLmlwZnMu'
-    'ZGh0LnJlZF9ibGFja190cmVlLlJlZEJsYWNrVHJlZU5vZGVSCnJpZ2h0Q2hpbGQ=');
+    'ChBSZWRCbGFja1RyZWVOb2RlEisKA2tleRgBIAEoCzIZLmlwZnMuZGh0LmNvbW1vbi5LX1BlZX'
+    'JJZFIDa2V5EjEKBXZhbHVlGAIgASgLMhsuaXBmcy5kaHQuY29tbW9uLlZfUGVlckluZm9SBXZh'
+    'bHVlEjAKBWNvbG9yGAMgASgOMhouaXBmcy5kaHQuY29tbW9uLk5vZGVDb2xvclIFY29sb3ISPQ'
+    'oEbGVmdBgEIAEoCzIpLmlwZnMuZGh0LnJlZF9ibGFja190cmVlLlJlZEJsYWNrVHJlZU5vZGVS'
+    'BGxlZnQSPwoFcmlnaHQYBSABKAsyKS5pcGZzLmRodC5yZWRfYmxhY2tfdHJlZS5SZWRCbGFja1'
+    'RyZWVOb2RlUgVyaWdodA==');
 
