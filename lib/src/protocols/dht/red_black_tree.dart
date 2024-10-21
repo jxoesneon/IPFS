074dbcde6858edca6e5dcde401ce45bf1d0b3a5c
joseeduardox <joseeduardox@gmail.com>
2024-10-21 16:17:24 +0000

    Refactor search related classes in Red-Black tree module
    In this commit, refactor the search related classes in the Red-Black tree module. Renamed the 'search' class to 'rb_search' and adjusted references accordingly. Additionally, created instances of insertion, deletion, and search classes in the RedBlackTree constructor for easier access to their functionalities.

--- lib/src/protocols/dht/red_black_tree.dart
+++ lib/src/protocols/dht/red_black_tree.dart
@@ -2,7 +2,7 @@
 import '/../src/proto/dht/common_tree.pb.dart' as common_tree;
 import 'red_black_tree/insertion.dart' as insertion;
 import 'red_black_tree/deletion.dart' as deletion;
-import 'red_black_tree/search.dart' as search;
+import 'red_black_tree/search.dart' as rb_search;
 import 'red_black_tree/rotations.dart' as rotations;
 import 'red_black_tree/fix_violations.dart' as fix_violations;
 
@@ -14,45 +14,55 @@ class RedBlackTreeNode<K_PeerId, V_PeerInfo> {
   RedBlackTreeNode<K_PeerId, V_PeerInfo>? left_child; // Renamed from 'left'
   RedBlackTreeNode<K_PeerId, V_PeerInfo>? right_child; // Renamed from 'right'
   RedBlackTreeNode<K_PeerId, V_PeerInfo>? parent;
-  
+
   // Constructor
-  RedBlackTreeNode(
-      this.key,
-      this.value,
+  RedBlackTreeNode(this.key, this.value,
       {this.color = common_tree.NodeColor.RED,
       this.left_child,
-      this.right_child});
-
-  get parent => null;
+      this.right_child,
+      this.parent});
 }
 
 class RedBlackTree<K_PeerId, V_PeerInfo> {
   RedBlackTreeNode<K_PeerId, V_PeerInfo>? _root; // Private root node
   int Function(K_PeerId, K_PeerId) _compare;
 
+  // Instances of insertion, deletion, and search classes
+  final insertion.Insertion<K_PeerId, V_PeerInfo> _insertion;
+  final deletion.Deletion<K_PeerId, V_PeerInfo> _deletion;
+  final rb_search.Search<K_PeerId, V_PeerInfo> _search;
+
   RedBlackTree({int Function(K_PeerId, K_PeerId)? compare})
-      : _compare = compare ?? ((a, b) => (a as int).compareTo(b as int));
+      : _compare = compare ?? ((a, b) => (a as int).compareTo(b as int)),
+        _insertion = insertion.Insertion<K_PeerId, V_PeerInfo>(),
+        _deletion = deletion.Deletion<K_PeerId, V_PeerInfo>(),
+        _search = rb_search.Search<K_PeerId, V_PeerInfo>();
 
   // Insert a new node with the given key and value into the tree.
   void insert(K_PeerId key_insert, V_PeerInfo value_insert) {
-    final newNode = RedBlackTreeNode(key_insert as common_tree.K_PeerId, value_insert as common_tree.V_PeerInfo);
-    insertion._insertNode(this, newNode);
+    final newNode = RedBlackTreeNode(key_insert as common_tree.K_PeerId,
+        value_insert as common_tree.V_PeerInfo);
+    _insertion.insertNode(this, newNode); // Use _insertion instance
   }
 
   // Delete the node with the given key from the tree.
   void delete(K_PeerId key) {
-    deletion._deleteNode(this, key);
+    _deletion.deleteNode(this, key); // Use _deletion instance
   }
 
   // Search for a node with the given key in the tree.
-  V_PeerInfo? search(K_PeerId key) {
-    final node = search._searchNode(this, key);
+  common_tree.V_PeerInfo? search(K_PeerId key) {
+    final node = _search.searchNode(this, key); // Use _search instance
     return node?.value;
   }
 
   // Public getter for the root node.
   RedBlackTreeNode<K_PeerId, V_PeerInfo>? get root => _root;
 
+  // Setter for the root node.
+  set root(RedBlackTreeNode<K_PeerId, V_PeerInfo>? newRoot) {
+    _root = newRoot;
+  }
+
   int compare(K_PeerId a, K_PeerId b) => _compare(a, b);
 }
-
