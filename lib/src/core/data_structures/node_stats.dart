3336a6e51960c1674a93fc6b642c00d174b47bd7
joseeduardox <joseeduardox@gmail.com>
2024-10-25 20:17:37 +0000

    Refactor NodeStats class to use Protobuf for serialization/deserialization

--- lib/src/core/data_structures/node_stats.dart
+++ lib/src/core/data_structures/node_stats.dart
@@ -1,4 +1,7 @@
-// lib/src/core/data_structures/node_stats.dart (create this file)
+// lib/src/core/data_structures/node_stats.dart
+
+import 'package:fixnum/fixnum.dart' as fixnum;
+import '/../src/proto/dht/node_stats.pb.dart' as proto; // Import the generated Protobuf file
 
 /// Represents statistics about the IPFS node.
 class NodeStats {
@@ -25,6 +28,27 @@ class NodeStats {
     required this.bandwidthSent,
     required this.bandwidthReceived,
   });
+
+  /// Creates a [NodeStats] instance from its Protobuf representation.
+  factory NodeStats.fromProto(proto.NodeStats pbNodeStats) {
+    return NodeStats(
+      numBlocks: pbNodeStats.numBlocks,
+      datastoreSize: pbNodeStats.datastoreSize.toInt(),
+      numConnectedPeers: pbNodeStats.numConnectedPeers,
+      bandwidthSent: pbNodeStats.bandwidthSent.toInt(),
+      bandwidthReceived: pbNodeStats.bandwidthReceived.toInt(),
+    );
+  }
+
+  /// Converts the [NodeStats] instance to its Protobuf representation.
+  proto.NodeStats toProto() {
+    return proto.NodeStats()
+      ..numBlocks = numBlocks
+      ..datastoreSize = fixnum.Int64(datastoreSize)
+      ..numConnectedPeers = numConnectedPeers
+      ..bandwidthSent = fixnum.Int64(bandwidthSent)
+      ..bandwidthReceived = fixnum.Int64(bandwidthReceived);
+  }
 
   @override
   String toString() {
