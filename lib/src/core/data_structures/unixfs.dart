77e60e7bd0af10e4ee81bb9247071384ce583478
joseeduardox <joseeduardox@gmail.com>
2024-10-25 21:19:03 +0000

    Refactor UnixFS class in unixfs.dart

--- lib/src/core/data_structures/unixfs.dart
+++ lib/src/core/data_structures/unixfs.dart
@@ -1,101 +1,78 @@
+// lib/src/core/data_structures/unixfs.dart
+
 import 'dart:typed_data';
 import 'package:fixnum/fixnum.dart' as fixnum;
-import 'package:protobuf/protobuf.dart';
-import '/../src/proto/unixfs/unixfs.pb.dart' as unixfs;
+import '/../src/proto/dht/unixfs.pb.dart' as proto; // Import the generated Protobuf file
 
-/// Represents a UnixFS data structure in IPFS.
-class UnixFSData {
-  final unixfs.DataType type;
+/// Represents a UnixFS node in IPFS.
+class UnixFS {
+  final UnixFSType type;
   final Uint8List data;
-  final int filesize;
+  final int blockSize;
+  final int fileSize;
   final List<int> blocksizes;
-  final int hashType;
-  final int fanout;
-  final int mode;
-  final UnixTime mtime;
 
-  /// Creates a new [UnixFSData] instance.
-  UnixFSData({
+  UnixFS({
     required this.type,
     required this.data,
-    required this.filesize,
+    required this.blockSize,
+    required this.fileSize,
     required this.blocksizes,
-    required this.hashType,
-    required this.fanout,
-    required this.mode,
-    required this.mtime,
   });
 
-  /// Creates a [UnixFSData] from its byte representation.
-  static UnixFSData fromBytes(Uint8List bytes) {
-    try {
-      // Deserialize the Protobuf data
-      final pbData = unixfs.Data.fromBuffer(bytes);
-      return UnixFSData(
-        type: pbData.type,
-        data: pbData.data,
-        filesize: pbData.filesize.toInt(),
-        blocksizes: pbData.blocksizes.map((e) => e.toInt()).toList(),
-        hashType: pbData.hashType.toInt(),
-        fanout: pbData.fanout.toInt(),
-        mode: pbData.mode.toInt(),
-        mtime: UnixTime.fromProto(pbData.mtime),
-      );
-    } catch (e) {
-      throw FormatException('Failed to parse UnixFSData from bytes: $e');
-    }
-  }
-
-  /// Converts the [UnixFSData] to its byte representation.
-  Uint8List toBytes() {
-    // Create a Protobuf Data object
-    final pbData = unixfs.Data()
-      ..type = type
-      ..data = data
-      ..filesize = fixnum.Int64(filesize)
-      ..blocksizes.addAll(blocksizes.map((e) => fixnum.Int64(e)))
-      ..hashType = fixnum.Int64(hashType)
-      ..fanout = fixnum.Int64(fanout)
-      ..mode = mode
-      ..mtime = mtime.toProto();
-
-    return pbData.writeToBuffer();
-  }
-
-  @override
-  String toString() {
-    return 'UnixFSData(type: $type, filesize: $filesize, blocksizes: $blocksizes, hashType: $hashType, fanout: $fanout, mode: $mode, mtime: $mtime)';
-  }
-}
-
-/// Represents a Unix timestamp with optional fractional nanoseconds.
-class UnixTime {
-  final int seconds;
-  final int fractionalNanoseconds;
-
-  /// Creates a new [UnixTime] instance.
-  UnixTime({
-    required this.seconds,
-    required this.fractionalNanoseconds,
-  });
-
-  /// Creates a [UnixTime] from its Protobuf representation.
-  static UnixTime fromProto(unixfs.UnixTime proto) {
-    return UnixTime(
-      seconds: proto.seconds.toInt(),
-      fractionalNanoseconds: proto.fractionalNanoseconds,
+  /// Creates a [UnixFS] from its Protobuf representation.
+  factory UnixFS.fromProto(proto.UnixFS pbUnixFS) {
+    return UnixFS(
+      type: _unixFSTypeFromProto(pbUnixFS.type),
+      data: Uint8List.fromList(pbUnixFS.data),
+      blockSize: pbUnixFS.blockSize.toInt(),
+      fileSize: pbUnixFS.fileSize.toInt(),
+      blocksizes: List<int>.from(pbUnixFS.blocksizes),
     );
   }
 
-  /// Converts the [UnixTime] to its Protobuf representation.
-  unixfs.UnixTime toProto() {
-    return unixfs.UnixTime()
-      ..seconds = fixnum.Int64(seconds)
-      ..fractionalNanoseconds = fractionalNanoseconds;
+  /// Converts the [UnixFS] to its Protobuf representation.
+  proto.UnixFS toProto() {
+    return proto.UnixFS()
+      ..type = _unixFSTypeToProto(type)
+      ..data = data
+      ..blockSize = fixnum.Int64(blockSize)
+      ..fileSize = fixnum.Int64(fileSize)
+      ..blocksizes.addAll(blocksizes);
   }
 
   @override
   String toString() {
-    return 'UnixTime(seconds: $seconds, fractionalNanoseconds: $fractionalNanoseconds)';
+    return 'UnixFS(type: $type, blockSize: $blockSize, fileSize: $fileSize, blocksizes: $blocksizes)';
   }
+
+  /// Converts a [proto.UnixFSTypeProto] to a [UnixFSType].
+  static UnixFSType _unixFSTypeFromProto(proto.UnixFSTypeProto protoType) {
+    switch (protoType) {
+      case proto.UnixFSTypeProto.FILE:
+        return UnixFSType.FILE;
+      case proto.UnixFSTypeProto.DIRECTORY:
+        return UnixFSType.DIRECTORY;
+      default:
+        throw ArgumentError('Unknown UnixFS type in protobuf: $protoType');
+    }
+  }
+
+  /// Converts a [UnixFSType] to a [proto.UnixFSTypeProto].
+  static proto.UnixFSTypeProto _unixFSTypeToProto(UnixFSType type) {
+    switch (type) {
+      case UnixFSType.FILE:
+        return proto.UnixFSTypeProto.FILE;
+      case UnixFSType.DIRECTORY:
+        return proto.UnixFSTypeProto.DIRECTORY;
+      default:
+        throw ArgumentError('Unknown UnixFS type: $type');
+    }
+  }
+}
+
+/// Enum representing the different types of UnixFS nodes.
+enum UnixFSType {
+  FILE,
+  DIRECTORY,
 }
