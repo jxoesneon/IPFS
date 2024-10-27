54d59412998c8c37b772c122faa4e053caa36af5
joseeduardox <joseeduardox@gmail.com>
2024-10-27 08:58:51 +0000

    Refactor IPFSNode pubSubHandler initialization - Import required module and update initialization of pubSubHandler in IPFSNode class.

--- lib/src/core/ipfs_node/ipfs_node.dart
+++ lib/src/core/ipfs_node/ipfs_node.dart
@@ -1,5 +1,6 @@
 // lib/src/core/ipfs_node/ipfs_node.dart
 
+import '../../transport/p2plib_router.dart';
 import 'bitswap_handler.dart';
 import 'dht_handler.dart';
 import 'pubsub_handler.dart';
@@ -11,19 +12,19 @@ import 'network_handler.dart';
 class IPFSNode {
   final BitswapHandler bitswapHandler;
   final DHTHandler dhtHandler;
-  final PubSubHandler pubSubHandler;
   final DatastoreHandler datastoreHandler;
   final RoutingHandler routingHandler;
   final NetworkHandler networkHandler;
+  late final PubSubHandler pubSubHandler;
 
   IPFSNode(config)
       : bitswapHandler = BitswapHandler(config),
         dhtHandler = DHTHandler(config),
-        pubSubHandler = PubSubHandler(config),
+        pubSubHandler = PubSubHandler(P2plibRouter(config), config.nodeId, config.networkEvents), // Pass both required parameters
         datastoreHandler = DatastoreHandler(config),
         routingHandler = RoutingHandler(config),
         networkHandler = NetworkHandler(config);
-
+        
   /// Starts the IPFS node.
   Future<void> start() async {
     await bitswapHandler.start();
