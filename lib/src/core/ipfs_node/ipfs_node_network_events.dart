9e7c8095b15ddf5d76f42cfd18f026744e6e1238
joseeduardox <joseeduardox@gmail.com>
2024-10-27 08:58:38 +0000

    Refactored network event listeners and added error event handling.

--- lib/src/core/ipfs_node/ipfs_node_network_events.dart
+++ lib/src/core/ipfs_node/ipfs_node_network_events.dart
@@ -1,7 +1,7 @@
 // lib/src/core/ipfs_node/ipfs_node_network_events.dart
-import 'dart:async';
 
-import '/../src/proto/transport/ipfs_node_network_events.pb.dart';
+import 'dart:async';
+import '/../src/proto/dht/ipfs_node_network_events.pb.dart';
 import '/../src/transport/circuit_relay_client.dart';
 import '/../src/transport/p2plib_router.dart';
 
@@ -20,102 +20,192 @@ class IpfsNodeNetworkEvents {
   void start() {
     _listenForRouterEvents();
     _listenForCircuitRelayEvents();
+    _listenForErrorEvents();
   }
 
+  /// Listens for router-related events and emits corresponding network events.
   void _listenForRouterEvents() {
+    _listenForConnectionEvents();
+    _listenForMessageEvents();
+    _listenForDHTEvents();
+    _listenForPubSubEvents();
+    _listenForStreamEvents();
+  }
+
+  /// Listens for connection events from the router.
+  void _listenForConnectionEvents() {
     _router.connectionEvents.listen((event) {
+      final networkEvent = NetworkEvent();
       switch (event.eventType) {
         case 'peer_connected':
-          _networkEventsController.add(NetworkEvent()
-            ..peerConnected = PeerConnectedEvent()
-              ..peerId = event.peerId
-              ..multiaddress = event.multiaddress);
+          networkEvent.peerConnected = PeerConnectedEvent()
+            ..peerId = event.peerId
+            ..multiaddress = event.multiaddress;
           break;
         case 'peer_disconnected':
-          _networkEventsController.add(NetworkEvent()
-            ..peerDisconnected = PeerDisconnectedEvent()
-              ..peerId = event.peerId);
+          networkEvent.peerDisconnected = PeerDisconnectedEvent()
+            ..peerId = event.peerId;
           break;
-        // Add more cases for other connection-related events if needed
       }
-    });
-
-    _router.messageEvents.listen((event) {
-      switch (event.eventType) {
-        case 'message_received':
-          _networkEventsController.add(NetworkEvent()
-            ..messageReceived = MessageReceivedEvent()
-              ..peerId = event.peerId
-              ..messageContent = event.messageContent);
-          break;
-        case 'message_sent':
-          _networkEventsController.add(NetworkEvent()
-            ..messageSent = MessageSentEvent()
-              ..peerId = event.peerId
-              ..messageContent = event.messageContent);
-          break;
-        // Add more cases for other message-related events if needed
-      }
-    });
-
-    // Listen for DHT-related events
-    _router.dhtEvents.listen((event) {
-      switch (event.eventType) {
-        case 'dht_value_found':
-          _networkEventsController.add(NetworkEvent()
-            ..dhtValueFound = DHTValueFoundEvent()
-              ..key = event.key
-              ..value = event.value);
-          break;
-        case 'dht_value_not_found':
-          _networkEventsController.add(NetworkEvent()
-            ..dhtValueNotFound = DHTValueNotFoundEvent()
-              ..key = event.key);
-          break;
-        // Add more cases for other DHT-related events if needed
-      }
-    });
-
-    // Listen for PubSub-related events
-    _router.pubSubEvents.listen((event) {
-      switch (event.eventType) {
-        case 'pubsub_message_received':
-          _networkEventsController.add(NetworkEvent()
-            ..pubSubMessageReceived = PubSubMessageReceivedEvent()
-              ..topic = event.topic
-              ..messageContent = event.messageContent);
-          break;
-        case 'pubsub_subscribed':
-          _networkEventsController.add(NetworkEvent()
-            ..pubSubSubscribed = PubSubSubscribedEvent()
-              ..topic = event.topic);
-          break;
-        // Add more cases for other PubSub-related events if needed
-      }
+      _networkEventsController.add(networkEvent);
     });
   }
 
+  /// Listens for message events from the router.
+  void _listenForMessageEvents() {
+    _router.messageEvents.listen((event) {
+      final networkEvent = NetworkEvent();
+      switch (event.eventType) {
+        case 'message_received':
+          networkEvent.messageReceived = MessageReceivedEvent()
+            ..peerId = event.peerId
+            ..messageContent = event.messageContent;
+          break;
+        case 'message_sent':
+          networkEvent.messageSent = MessageSentEvent()
+            ..peerId = event.peerId
+            ..messageContent = event.messageContent;
+          break;
+      }
+      _networkEventsController.add(networkEvent);
+    });
+  }
+
+  /// Listens for DHT-related events from the router.
+  void _listenForDHTEvents() {
+    _router.dhtEvents.listen((event) {
+      final networkEvent = NetworkEvent();
+      switch (event.eventType) {
+        case 'dht_value_found':
+          networkEvent.dhtValueFound = DHTValueFoundEvent()
+            ..key = event.key
+            ..value = event.value
+            ..peerId = event.peerId; // Added to match the proto
+          break;
+        case 'dht_value_not_found':
+          networkEvent.dhtValueNotFound = DHTValueNotFoundEvent()
+            ..key = event.key;
+          break;
+        case 'dht_provider_added':
+          networkEvent.dhtProviderAdded = DHTProviderAddedEvent()
+            ..key = event.key
+            ..peerId = event.peerId; // Added to match the proto
+          break;
+        case 'dht_provider_queried':
+          networkEvent.dhtProviderQueried = DHTProviderQueriedEvent()
+            ..key = event.key
+            ..providers.addAll(event.providers); // List of providers
+          break;
+      }
+      _networkEventsController.add(networkEvent);
+    });
+  }
+
+  /// Listens for PubSub-related events from the router.
+  void _listenForPubSubEvents() {
+    _router.pubSubEvents.listen((event) {
+      final networkEvent = NetworkEvent();
+      switch (event.eventType) {
+        case 'pubsub_message_received':
+          networkEvent.pubsubMessageReceived = PubsubMessageReceivedEvent()
+            ..topic = event.topic
+            ..messageContent = event.messageContent
+            ..peerId = event.peerId; // Added to match the proto
+          break;
+        case 'pubsub_subscribed':
+          networkEvent.pubsubSubscriptionCreated = PubsubSubscriptionCreatedEvent()
+            ..topic = event.topic;
+          break;
+        case 'pubsub_unsubscribed':
+          networkEvent.pubsubSubscriptionCancelled = PubsubSubscriptionCancelledEvent()
+            ..topic = event.topic;
+          break;
+      }
+      _networkEventsController.add(networkEvent);
+    });
+  }
+
+  /// Listens for stream-related events.
+  void _listenForStreamEvents() {
+    // Assuming there's a StreamEvents class in the router.
+    _router.streamEvents.listen((event) {
+      final networkEvent = NetworkEvent();
+      switch (event.eventType) {
+        case 'stream_started':
+          networkEvent.streamStarted = StreamStartedEvent()
+            ..streamId = event.streamId
+            ..peerId = event.peerId;
+          break;
+        case 'stream_ended':
+          networkEvent.streamEnded = StreamEndedEvent()
+            ..streamId = event.streamId
+            ..peerId = event.peerId
+            ..reason = event.reason;
+          break;
+        case 'peer_discovered':
+          networkEvent.peerDiscovered = PeerDiscoveredEvent()
+            ..peerId = event.peerId;
+          break;
+      }
+      _networkEventsController.add(networkEvent);
+    });
+  }
+
+  /// Listens for circuit relay-related events and emits corresponding network events.
   void _listenForCircuitRelayEvents() {
     _circuitRelayClient.connectionEvents.listen((event) {
+      final networkEvent = NetworkEvent();
       switch (event.eventType) {
         case 'circuit_relay_created':
-          _networkEventsController.add(NetworkEvent()
-            ..circuitRelayCreated = CircuitRelayCreatedEvent()
-              ..relayAddress = event.relayAddress);
+          networkEvent.circuitRelayCreated = CircuitRelayCreatedEvent()
+            ..relayAddress = event.relayAddress;
           break;
         case 'circuit_relay_failed':
-          _networkEventsController.add(NetworkEvent()
-            ..circuitRelayFailed = CircuitRelayFailedEvent()
-              ..relayAddress = event.relayAddress
-              ..errorMessage = event.errorMessage);
+          networkEvent.circuitRelayFailed = CircuitRelayFailedEvent()
+            ..relayAddress = event.relayAddress
+            ..errorMessage = event.errorMessage;
           break;
-        // Add more cases for other circuit relay-related events if needed
+        case 'circuit_relay_closed':
+          networkEvent.circuitRelayClosed = CircuitRelayClosedEvent()
+            ..relayAddress = event.relayAddress
+            ..reason = event.reason;
+          break;
+        case 'circuit_relay_traffic':
+          networkEvent.circuitRelayTraffic = CircuitRelayTrafficEvent()
+            ..relayAddress = event.relayAddress
+            ..dataSize = event.dataSize;
+          break;
+        case 'circuit_relay_data_received':
+          networkEvent.circuitRelayDataReceived = CircuitRelayDataReceivedEvent()
+            ..relayAddress = event.relayAddress
+            ..dataSize = event.dataSize;
+          break;
+        case 'circuit_relay_data_sent':
+          networkEvent.circuitRelayDataSent = CircuitRelayDataSentEvent()
+            ..relayAddress = event.relayAddress
+            ..dataSize = event.dataSize;
+          break;
       }
+      _networkEventsController.add(networkEvent);
+    });
+  }
+
+  /// Listens for error events and emits corresponding network error events.
+  void _listenForErrorEvents() {
+    _router.errorEvents.listen((event) {
+      final networkEvent = NetworkEvent()
+        ..error = ErrorEvent()
+        ..errorType = event.errorType
+        ..message = event.message
+        ..stackTrace = event.stackTrace;
+      _networkEventsController.add(networkEvent);
     });
   }
 
   /// Stops listening for network events and closes the stream controller.
   void dispose() {
-    _networkEventsController.close();
+    if (!_networkEventsController.isClosed) {
+      _networkEventsController.close();
+    }
   }
 }
