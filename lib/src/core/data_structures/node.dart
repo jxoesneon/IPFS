2e33743a295d1faaf20cb7eaaffb06ed3de5006c
Eduardo Rojas <joseeduardox@gmail.com>
2024-11-04 19:51:54 -0600

    ✨ feat(Node): enhance node structure with additional functionalities - update node properties to use more specific types and defaults - add factory methods for creating file and directory nodes - implement methods to add and remove links dynamically - add method to update node metadata and retrieve links by name♻️ refactor(node.dart): improve node structure for better clarity and usability - refactor node attributes and constructors for consistency - enhance protobuf conversions for simplified integration - add methods to manage links more effectively - update documentation for clarity on usage and parameters - improve readability and maintainability by organizing code structure
    📝 feat(UnixFS): introduce UnixFS and related handling mechanisms
    - establish `UnixFSTypeProto` for categorizing file types
    - implement `UnixFSProto` message structure
    - include factory methods for creating UnixFS instances from protobuf
    - manage block data and metadata for efficient processing
    
    🔧 feat(OperationLog): introduce operation logs for tracking changes
    - establish `OperationLogEntryProto` and `OperationLogProto` for structured logging
    - implement methods to handle log entries and operations effectively
    
    🔧 feat(Pin): enhance pin management in IPFS
    - refactor pin handling with updated structure and fields
    - implement pin management operations to support direct and recursive types
    
    🔧 feat(DHT): improve DHT functionality with comprehensive routing, peer management
    - implement `DHTClient` and related classes for structured peer operations
    - establish enhanced routing mechanism using Kademlia principles
    
    🔧 config: enhance gateway services for better interaction and caching
    - implement `GatewayHandler` and `CachedPreviewGenerator` for optimized response handling
    - establish directory parsing for directory listings in IPFS
    - provide adaptive caching for stored previews based on compression
    
    📝 docs: improve code documentation across services and protocols
    - clarify purpose and structure within various service and protocol implementations

--- lib/src/core/data_structures/node.dart
+++ lib/src/core/data_structures/node.dart
@@ -1,82 +1,165 @@
-// lib/src/core/data_structures/node.dart
-
+import 'cid.dart';
+import 'link.dart';
 import 'dart:typed_data';
-import 'package:fixnum/fixnum.dart' as fixnum;
-import '../../proto/generated/core/node.pb.dart'
-    as proto; // Import the generated Protobuf file
+import 'package:fixnum/fixnum.dart';
+import '../../proto/generated/core/node.pb.dart';
 import '../../proto/generated/core/node_type.pbenum.dart';
-import 'cid.dart'; // Use the CID class for handling CIDs
-import 'link.dart'; // Use the Link class for handling links
-import 'node_type.dart'; // Use the NodeType enum
 
 /// Represents a node in the IPFS Merkle DAG.
 class Node {
-  final CID cid; // Use CID class
+  /// The content identifier of the node
+  final CID cid;
+  
+  /// Links to other nodes
   final List<Link> links;
+  
+  /// Raw data stored in the node
   final Uint8List data;
-  final NodeType type;
-  final int size;
-  final int timestamp;
+  
+  /// Type of the node
+  final NodeTypeProto type;
+  
+  /// Size of the node in bytes
+  final Int64 size;
+  
+  /// Timestamp of the node's last modification
+  final Int64 timestamp;
+  
+  /// Additional metadata associated with the node
   final Map<String, String> metadata;
 
+  /// Creates a new [Node] instance.
   Node({
     required this.cid,
-    required this.links,
-    required this.data,
-    required this.type,
-    required this.size,
-    required this.timestamp,
-    required this.metadata,
-  });
+    this.links = const [],
+    Uint8List? data,
+    this.type = NodeTypeProto.NODE_TYPE_UNSPECIFIED,
+    Int64? size,
+    Int64? timestamp,
+    this.metadata = const {},
+  })  : data = data ?? Uint8List(0),
+        size = size ?? Int64(0),
+        timestamp = timestamp ?? Int64(DateTime.now().millisecondsSinceEpoch);
 
   /// Creates a [Node] from its Protobuf representation.
-  static Node fromProto(proto.Node pbNode) {
+  factory Node.fromProto(NodeProto proto) {
     return Node(
-      cid: CID.fromProto(pbNode.cid),
-      links: pbNode.links.map((link) => Link.fromProto(link)).toList(),
-      data: Uint8List.fromList(pbNode.data), // Ensure data is copied
-      type: NodeTypeExtension.fromName(pbNode.type.name),
-      size: pbNode.size.toInt(),
-      timestamp: pbNode.timestamp.toInt(),
-      metadata: Map<String, String>.from(pbNode.metadata),
+      cid: CID.fromProto(proto.cid),
+      links: proto.links.map((link) => Link.fromProto(link)).toList(),
+      data: Uint8List.fromList(proto.data),
+      type: proto.type,
+      size: proto.size,
+      timestamp: proto.timestamp,
+      metadata: Map<String, String>.from(proto.metadata),
     );
   }
 
   /// Converts the [Node] to its Protobuf representation.
-  proto.Node toProto() {
-    final pbNode = proto.Node()
-      ..cid = cid.toProto()
-      ..data = data
-      ..type = NodeTypeProto.valueOf(type.index)! // Corrected line
-      ..size = fixnum.Int64(size)
-      ..timestamp = fixnum.Int64(timestamp);
+  NodeProto toProto() => NodeProto()
+    ..cid = cid.toProto()
+    ..links.addAll(links.map((link) => link.toProto()))
+    ..data = data
+    ..type = type
+    ..size = size
+    ..timestamp = timestamp
+    ..metadata.addAll(metadata);
 
-    pbNode.links.addAll(links.map((link) => link.toProto()));
-    pbNode.metadata.addAll(metadata);
-
-    return pbNode;
+  /// Creates a file node with the given [cid], [data], and [metadata].
+  factory Node.file({
+    required CID cid,
+    required Uint8List data,
+    Map<String, String> metadata = const {},
+  }) {
+    return Node(
+      cid: cid,
+      data: data,
+      type: NodeTypeProto.NODE_TYPE_FILE,
+      metadata: metadata,
+      size: Int64(data.length),
+    );
   }
 
-  /// Converts the [Node] to a byte array (Uint8List).
-  Uint8List toBytes() {
-    return toProto()
-        .writeToBuffer(); // Serialize using Protobuf's writeToBuffer method
+  /// Creates a directory node with the given [cid], [links], and [metadata].
+  factory Node.directory({
+    required CID cid,
+    required List<Link> links,
+    Map<String, String> metadata = const {},
+  }) {
+    return Node(
+      cid: cid,
+      links: links,
+      type: NodeTypeProto.NODE_TYPE_DIRECTORY,
+      metadata: metadata,
+      size: Int64(links.fold(0, (sum, link) => sum + link.size.toInt())),
+    );
   }
 
-  Map<String, dynamic> toMap() {
-    return {
-      'data': data,
-      'links': links,
-      'size': size,
-      'timestamp': timestamp,
-      'metadata': metadata,
-      'cid': cid,
-      'type': type,
-    };
+  /// Adds a [link] to the node if it's a directory node.
+  /// 
+  /// Throws a [StateError] if the node is not a directory.
+  Node addLink(Link link) {
+    if (type != NodeTypeProto.NODE_TYPE_DIRECTORY) {
+      throw StateError('Can only add links to directory nodes');
+    }
+
+    return Node(
+      cid: cid,
+      links: [...links, link],
+      type: type,
+      metadata: metadata,
+      size: size + link.size,
+      timestamp: timestamp,
+    );
+  }
+
+  /// Removes a link with the given [name] from the node if it's a directory node.
+  /// 
+  /// Throws a [StateError] if the node is not a directory.
+  /// Throws an [ArgumentError] if the link is not found.
+  Node removeLink(String name) {
+    if (type != NodeTypeProto.NODE_TYPE_DIRECTORY) {
+      throw StateError('Can only remove links from directory nodes');
+    }
+
+    final linkToRemove = links.firstWhere(
+      (link) => link.name == name,
+      orElse: () => throw ArgumentError('Link not found: $name'),
+    );
+
+    return Node(
+      cid: cid,
+      links: links.where((link) => link.name != name).toList(),
+      type: type,
+      metadata: metadata,
+      size: size - linkToRemove.size,
+      timestamp: timestamp,
+    );
+  }
+
+  /// Gets a link by [name].
+  /// 
+  /// Returns null if the link is not found.
+  Link? getLink(String name) {
+    try {
+      return links.firstWhere((link) => link.name == name);
+    } on StateError {
+      return null;
+    }
+  }
+
+  /// Updates the node's metadata with [newMetadata].
+  Node updateMetadata(Map<String, String> newMetadata) {
+    return Node(
+      cid: cid,
+      links: links,
+      data: data,
+      type: type,
+      size: size,
+      timestamp: Int64(DateTime.now().millisecondsSinceEpoch),
+      metadata: {...metadata, ...newMetadata},
+    );
   }
 
   @override
-  String toString() {
-    return 'Node(cid: $cid, type: $type, size: $size, timestamp: $timestamp, metadata: $metadata)';
-  }
+  String toString() => 'Node(cid: $cid, type: $type, size: $size, links: ${links.length})';
 }
