399b76ee62cf1b379a8e222ddfbe6b3d7b3f779d
Eduardo Rojas <joseeduardox@gmail.com>
2024-11-04 19:55:31 -0600

    ✨ feat(delegated_routing): add DelegatedRoutingHandler class skeleton - create initial structure for delegated routing handling based on API spec

---
+++ lib/src/routing/delegated_routing.dart
@@ -0,0 +1,6 @@
+class DelegatedRoutingHandler {
+  // TODO: Implement according to Delegated Routing V1 HTTP API spec
+  Future<RoutingResponse> findProviders(CID cid) async {
+    // Implementation following the spec
+  }
+}
