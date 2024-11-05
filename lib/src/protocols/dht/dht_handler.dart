ea84f352778ae4958aed9ca61dde90f66db5ba42
Eduardo Rojas <joseeduardox@gmail.com>
2024-11-04 19:54:53 -0600

    ✨ feat(dht_handler.dart): add DHTHandler class for DHT protocol operations - define required methods for peer management and data handling

---
+++ lib/src/protocols/dht/dht_handler.dart
@@ -0,0 +1,12 @@
+class DHTHandler {
+  // Required operations
+  Future<List<PeerInfo>> findPeer(PeerID id);
+  Future<void> provide(CID cid);
+  Future<List<PeerInfo>> findProviders(CID cid);
+  Future<void> putValue(Key key, Value value);
+  Future<Value> getValue(Key key);
+
+  // Required behaviors
+  Future<void> handleRoutingTableUpdate(PeerInfo peer);
+  Future<void> handleProvideRequest(CID cid, PeerID provider);
+}
