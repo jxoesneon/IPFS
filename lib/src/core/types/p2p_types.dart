6ffafacad1afe2620670f0bf4543e3cdd286bc1e
Eduardo Rojas <joseeduardox@gmail.com>
2024-11-07 04:48:56 -0600

    ✨ feat(p2p): add type aliases for p2plib types
    - Introduce type aliases for commonly used p2plib types
    - Enhance code readability and maintainability

---
+++ lib/src/core/types/p2p_types.dart
@@ -0,0 +1,7 @@
+import 'package:p2plib/p2plib.dart' as p2p;
+
+// Type aliases for commonly used p2plib types
+typedef LibP2PPeerId = p2p.PeerId;
+typedef LibP2PFullAddress = p2p.FullAddress;
+typedef LibP2PPacket = p2p.Packet;
+typedef LibP2PRouterL0 = p2p.RouterL0;
