0456335441487911e358420df5c9d283dad67310
Eduardo Rojas <joseeduardox@gmail.com>
2024-11-07 04:46:28 -0600

    ✨ feat(core): add block interfaces
    - Introduce IBlock and IBlockFactory interfaces for block handling
    - Enable structured access and conversion methods for blocks

---
+++ lib/src/core/interfaces/block.dart
@@ -0,0 +1,23 @@
+import 'dart:typed_data';
+import 'package:dart_ipfs/src/core/cid.dart';
+import 'package:dart_ipfs/src/proto/generated/core/block.pb.dart';
+import 'package:dart_ipfs/src/proto/generated/bitswap/bitswap.pb.dart'
+    as bitswap_pb;
+
+abstract class IBlock {
+  Uint8List get data;
+  CID get cid;
+  int get size;
+
+  BlockProto toProto();
+  bitswap_pb.Block toBitswapProto();
+  Uint8List toBytes();
+  bool validate();
+}
+
+abstract class IBlockFactory<T extends IBlock> {
+  T fromProto(BlockProto proto);
+  T fromBitswapProto(bitswap_pb.Block proto);
+  T fromBytes(Uint8List bytes);
+  T fromData(Uint8List data, CID cid);
+}
