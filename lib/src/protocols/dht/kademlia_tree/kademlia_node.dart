f64bea193885e8c1d0aa3025ae13624db829357c
Eduardo Rojas <joseeduardox@gmail.com>
2024-11-09 02:22:10 -0600

    ♻️ refactor(kademlia_node): restructure KademliaNode properties
    - Changed associatedPeerId and lastSeen to private variables
    - Simplified constructor parameters for better readability
    - Updated getters for associatedPeerId and lastSeen to match new structure

--- lib/src/protocols/dht/kademlia_tree/kademlia_node.dart
+++ lib/src/protocols/dht/kademlia_tree/kademlia_node.dart
@@ -1,24 +1,22 @@
 import 'package:p2plib/p2plib.dart' as p2p;
-import '../../../proto/generated/dht/common_kademlia.pb.dart';
-import '../../../proto/generated/dht/kademlia_node.pb.dart' as kademlia_node_pb;
-// lib/src/protocols/dht/kademlia_tree/kademlia_node.dart
-    as common_kademlia_pb;
 
 class KademliaNode {
   final p2p.PeerId peerId;
-  final int distance; // XOR distance from the local node
-  final List<KademliaNode> children; // Child nodes for branching
-  int? bucketIndex; // Reference to the bucket the node belongs to
+  final int distance;
+  final List<KademliaNode> children;
+  final p2p.PeerId _associatedPeerId;
+  final int _lastSeen;
+  int? bucketIndex;
 
-  KademliaNode(this.peerId, this.distance, p2p.PeerId associatedPeerId,
-      {required int lastSeen})
-      : children = [];
+  KademliaNode(
+    this.peerId,
+    this.distance,
+    this._associatedPeerId, {
+    required int lastSeen,
+  })  : _lastSeen = lastSeen,
+        children = [];
 
-  get lastSeen => null;
+  int get lastSeen => _lastSeen;
 
-  p2p.PeerId get associatedPeerId => null;
-
-  // Add any other necessary properties or methods for your node here.
-  // For example, you might want to store additional data associated with the node,
-  // or implement methods for updating the node's state.
+  p2p.PeerId get associatedPeerId => _associatedPeerId;
 }
