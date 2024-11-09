bfe322c9f4e59dcf9fc24ecfae02bb48a2e619f5
Eduardo Rojas <joseeduardox@gmail.com>
2024-11-09 02:23:03 -0600

    ✨ feat(kademlia_tree): enhance add peer functionality
    - Add lastSeen timestamp to the KademliaNode for better tracking
    - Update import paths to reflect new package structure
    - Improve code readability with formatted node creation

--- lib/src/protocols/dht/kademlia_tree/add_peer.dart
+++ lib/src/protocols/dht/kademlia_tree/add_peer.dart
@@ -1,9 +1,9 @@
-// lib/src/protocols/dht/kademlia_tree/add_peer.dart
 import 'package:p2plib/p2plib.dart' as p2p;
-import 'helpers.dart' as helpers;
-import 'kademlia_node.dart';
-import '../kademlia_tree.dart';
-import 'bucket_management.dart';
+import 'package:dart_ipfs/src/protocols/dht/kademlia_tree.dart';
+import 'package:dart_ipfs/src/protocols/dht/kademlia_tree/kademlia_node.dart';
+import 'package:dart_ipfs/src/protocols/dht/kademlia_tree/bucket_management.dart';
+import 'package:dart_ipfs/src/protocols/dht/kademlia_tree/helpers.dart' as helpers;
+// lib/src/protocols/dht/kademlia_tree/add_peer.dart
 
 extension AddPeer on KademliaTree {
   // Add a peer to the Kademlia tree
@@ -12,7 +12,12 @@ extension AddPeer on KademliaTree {
     int distance = helpers.calculateDistance(peerId, this.root!.peerId);
     int bucketIndex = helpers.getBucketIndex(distance);
 
-    KademliaNode newNode = KademliaNode(peerId, distance, associatedPeerId);
+    KademliaNode newNode = KademliaNode(
+      peerId, 
+      distance, 
+      associatedPeerId,
+      lastSeen: DateTime.now().millisecondsSinceEpoch,
+    );
     newNode.bucketIndex = bucketIndex;
 
     // Insert into the RedBlackTree
@@ -20,7 +25,7 @@ extension AddPeer on KademliaTree {
 
     // Handle bucket fullness - splitting or replacement
     if (this.buckets[bucketIndex].size > KademliaTree.kBucketSize) {
-      handleBucketFullness(bucketIndex, peerId, associatedPeerId); // Call the getter
+      handleBucketFullness(bucketIndex, peerId, associatedPeerId);
     }
   }
 }
