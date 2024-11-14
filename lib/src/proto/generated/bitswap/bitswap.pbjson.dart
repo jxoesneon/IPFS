88a765b0d6e4ffa7a35b31725b37ef069d20c1e5
Eduardo Rojas <joseeduardox@gmail.com>
2024-11-14 03:46:42 -0600

    ♻️ refactor(proto): rename WantType to MessageType for clarity
    - update variable name from WantType to MessageType
    - adjust related enums and descriptors to match the new naming
    - ensure backward compatibility by maintaining deprecated annotations
    
    ✨ feat(proto): add new fields to Block and Message definitions
    
    - add cid and found fields to Block for enhanced identification
    - introduce message_id field in Message for unique message tracking
    - retain existing fields while expanding the message structure

--- lib/src/proto/generated/bitswap/bitswap.pbjson.dart
+++ lib/src/proto/generated/bitswap/bitswap.pbjson.dart
@@ -13,39 +13,43 @@ import 'dart:convert' as $convert;
 import 'dart:core' as $core;
 import 'dart:typed_data' as $typed_data;
 
-@$core.Deprecated('Use wantTypeDescriptor instead')
-const WantType$json = {
-  '1': 'WantType',
+@$core.Deprecated('Use messageTypeDescriptor instead')
+const MessageType$json = {
+  '1': 'MessageType',
   '2': [
-    {'1': 'WANT_TYPE_UNSPECIFIED', '2': 0},
-    {'1': 'WANT_TYPE_BLOCK', '2': 1},
-    {'1': 'WANT_TYPE_HAVE', '2': 2},
+    {'1': 'MESSAGE_TYPE_UNKNOWN', '2': 0},
+    {'1': 'MESSAGE_TYPE_WANT_BLOCK', '2': 1},
+    {'1': 'MESSAGE_TYPE_WANT_HAVE', '2': 2},
+    {'1': 'MESSAGE_TYPE_BLOCK', '2': 3},
+    {'1': 'MESSAGE_TYPE_HAVE', '2': 4},
+    {'1': 'MESSAGE_TYPE_DONT_HAVE', '2': 5},
   ],
 };
 
-/// Descriptor for `WantType`. Decode as a `google.protobuf.EnumDescriptorProto`.
-final $typed_data.Uint8List wantTypeDescriptor = $convert.base64Decode(
-    'CghXYW50VHlwZRIZChVXQU5UX1RZUEVfVU5TUEVDSUZJRUQQABITCg9XQU5UX1RZUEVfQkxPQ0'
-    'sQARISCg5XQU5UX1RZUEVfSEFWRRAC');
+/// Descriptor for `MessageType`. Decode as a `google.protobuf.EnumDescriptorProto`.
+final $typed_data.Uint8List messageTypeDescriptor = $convert.base64Decode(
+    'CgtNZXNzYWdlVHlwZRIYChRNRVNTQUdFX1RZUEVfVU5LTk9XThAAEhsKF01FU1NBR0VfVFlQRV'
+    '9XQU5UX0JMT0NLEAESGgoWTUVTU0FHRV9UWVBFX1dBTlRfSEFWRRACEhYKEk1FU1NBR0VfVFlQ'
+    'RV9CTE9DSxADEhUKEU1FU1NBR0VfVFlQRV9IQVZFEAQSGgoWTUVTU0FHRV9UWVBFX0RPTlRfSE'
+    'FWRRAF');
 
 @$core.Deprecated('Use wantlistEntryDescriptor instead')
 const WantlistEntry$json = {
   '1': 'WantlistEntry',
   '2': [
-    {'1': 'block', '3': 1, '4': 1, '5': 12, '10': 'block'},
+    {'1': 'cid', '3': 1, '4': 1, '5': 12, '10': 'cid'},
     {'1': 'priority', '3': 2, '4': 1, '5': 5, '10': 'priority'},
     {'1': 'cancel', '3': 3, '4': 1, '5': 8, '10': 'cancel'},
-    {'1': 'wantType', '3': 4, '4': 1, '5': 14, '6': '.bitswap.WantType', '10': 'wantType'},
+    {'1': 'type', '3': 4, '4': 1, '5': 14, '6': '.bitswap.MessageType', '10': 'type'},
     {'1': 'sendDontHave', '3': 5, '4': 1, '5': 8, '10': 'sendDontHave'},
   ],
 };
 
 /// Descriptor for `WantlistEntry`. Decode as a `google.protobuf.DescriptorProto`.
 final $typed_data.Uint8List wantlistEntryDescriptor = $convert.base64Decode(
-    'Cg1XYW50bGlzdEVudHJ5EhQKBWJsb2NrGAEgASgMUgVibG9jaxIaCghwcmlvcml0eRgCIAEoBV'
-    'IIcHJpb3JpdHkSFgoGY2FuY2VsGAMgASgIUgZjYW5jZWwSLQoId2FudFR5cGUYBCABKA4yES5i'
-    'aXRzd2FwLldhbnRUeXBlUgh3YW50VHlwZRIiCgxzZW5kRG9udEhhdmUYBSABKAhSDHNlbmREb2'
-    '50SGF2ZQ==');
+    'Cg1XYW50bGlzdEVudHJ5EhAKA2NpZBgBIAEoDFIDY2lkEhoKCHByaW9yaXR5GAIgASgFUghwcm'
+    'lvcml0eRIWCgZjYW5jZWwYAyABKAhSBmNhbmNlbBIoCgR0eXBlGAQgASgOMhQuYml0c3dhcC5N'
+    'ZXNzYWdlVHlwZVIEdHlwZRIiCgxzZW5kRG9udEhhdmUYBSABKAhSDHNlbmREb250SGF2ZQ==');
 
 @$core.Deprecated('Use wantlistDescriptor instead')
 const Wantlist$json = {
@@ -65,14 +69,17 @@ final $typed_data.Uint8List wantlistDescriptor = $convert.base64Decode(
 const Block$json = {
   '1': 'Block',
   '2': [
-    {'1': 'prefix', '3': 1, '4': 1, '5': 12, '10': 'prefix'},
+    {'1': 'cid', '3': 1, '4': 1, '5': 12, '10': 'cid'},
     {'1': 'data', '3': 2, '4': 1, '5': 12, '10': 'data'},
+    {'1': 'found', '3': 3, '4': 1, '5': 8, '10': 'found'},
+    {'1': 'format', '3': 4, '4': 1, '5': 9, '10': 'format'},
   ],
 };
 
 /// Descriptor for `Block`. Decode as a `google.protobuf.DescriptorProto`.
 final $typed_data.Uint8List blockDescriptor = $convert.base64Decode(
-    'CgVCbG9jaxIWCgZwcmVmaXgYASABKAxSBnByZWZpeBISCgRkYXRhGAIgASgMUgRkYXRh');
+    'CgVCbG9jaxIQCgNjaWQYASABKAxSA2NpZBISCgRkYXRhGAIgASgMUgRkYXRhEhQKBWZvdW5kGA'
+    'MgASgIUgVmb3VuZBIWCgZmb3JtYXQYBCABKAlSBmZvcm1hdA==');
 
 @$core.Deprecated('Use blockPresenceDescriptor instead')
 const BlockPresence$json = {
@@ -103,15 +110,19 @@ final $typed_data.Uint8List blockPresenceDescriptor = $convert.base64Decode(
 const Message$json = {
   '1': 'Message',
   '2': [
-    {'1': 'wantlist', '3': 1, '4': 1, '5': 11, '6': '.bitswap.Wantlist', '10': 'wantlist'},
-    {'1': 'blocks', '3': 2, '4': 3, '5': 11, '6': '.bitswap.Block', '10': 'blocks'},
-    {'1': 'blockPresences', '3': 3, '4': 3, '5': 11, '6': '.bitswap.BlockPresence', '10': 'blockPresences'},
+    {'1': 'message_id', '3': 1, '4': 1, '5': 9, '10': 'messageId'},
+    {'1': 'type', '3': 2, '4': 1, '5': 14, '6': '.bitswap.MessageType', '10': 'type'},
+    {'1': 'wantlist', '3': 3, '4': 1, '5': 11, '6': '.bitswap.Wantlist', '10': 'wantlist'},
+    {'1': 'blocks', '3': 4, '4': 3, '5': 11, '6': '.bitswap.Block', '10': 'blocks'},
+    {'1': 'blockPresences', '3': 5, '4': 3, '5': 11, '6': '.bitswap.BlockPresence', '10': 'blockPresences'},
   ],
 };
 
 /// Descriptor for `Message`. Decode as a `google.protobuf.DescriptorProto`.
 final $typed_data.Uint8List messageDescriptor = $convert.base64Decode(
-    'CgdNZXNzYWdlEi0KCHdhbnRsaXN0GAEgASgLMhEuYml0c3dhcC5XYW50bGlzdFIId2FudGxpc3'
-    'QSJgoGYmxvY2tzGAIgAygLMg4uYml0c3dhcC5CbG9ja1IGYmxvY2tzEj4KDmJsb2NrUHJlc2Vu'
-    'Y2VzGAMgAygLMhYuYml0c3dhcC5CbG9ja1ByZXNlbmNlUg5ibG9ja1ByZXNlbmNlcw==');
+    'CgdNZXNzYWdlEh0KCm1lc3NhZ2VfaWQYASABKAlSCW1lc3NhZ2VJZBIoCgR0eXBlGAIgASgOMh'
+    'QuYml0c3dhcC5NZXNzYWdlVHlwZVIEdHlwZRItCgh3YW50bGlzdBgDIAEoCzIRLmJpdHN3YXAu'
+    'V2FudGxpc3RSCHdhbnRsaXN0EiYKBmJsb2NrcxgEIAMoCzIOLmJpdHN3YXAuQmxvY2tSBmJsb2'
+    'NrcxI+Cg5ibG9ja1ByZXNlbmNlcxgFIAMoCzIWLmJpdHN3YXAuQmxvY2tQcmVzZW5jZVIOYmxv'
+    'Y2tQcmVzZW5jZXM=');
 
