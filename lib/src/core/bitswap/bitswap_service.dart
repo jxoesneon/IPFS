4503a13c957a7783c3e465d04ea9c098d6a6a809
Jose Eduardo Rojas Jimenez <joseeduardox@gmail.com>
2025-12-12 16:33:27 -0600

    refactor: Update protobuf definitions, introduce RPC services, and add protocol tests.

--- lib/src/core/bitswap/bitswap_service.dart
+++ lib/src/core/bitswap/bitswap_service.dart
@@ -2,11 +2,11 @@ import '../data_structures/block.dart';
 import '../../proto/generated/bitswap/bitswap.pb.dart' as proto;
 
 class BitswapService {
-  proto.Block convertToProtoBlock(Block block) {
+  proto.Message_Block convertToProtoBlock(Block block) {
     return block.toBitswapProto();
   }
 
-  Block convertFromProtoBlock(proto.Block protoBlock) {
-    return Block.fromBitswapProto(protoBlock);
+  Future<Block> convertFromProtoBlock(proto.Message_Block protoBlock) async {
+    return await Block.fromBitswapProto(protoBlock);
   }
 }
