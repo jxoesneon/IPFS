5a8f1194ad711e87f18c56b30a66d39d18ee26a6
Jose Eduardo Rojas Jimenez <joseeduardox@gmail.com>
2025-12-12 16:41:59 -0600

    feat: Add Kademlia DHT and IPNS protobuf definitions, update existing protobufs, and introduce RPC server and local crypto.

--- lib/src/core/messages/message_factory.dart
+++ lib/src/core/messages/message_factory.dart
@@ -1,9 +1,8 @@
 import 'dart:typed_data';
 
-import 'package:dart_ipfs/src/proto/generated/google/protobuf/timestamp.pb.dart';
+import 'package:protobuf/well_known_types/google/protobuf/timestamp.pb.dart';
 import 'package:fixnum/fixnum.dart';
 import 'package:dart_ipfs/src/proto/generated/base_messages.pb.dart';
-import 'package:dart_ipfs/src/proto/generated/dht_messages.pb.dart';
 
 class MessageFactory {
   static IPFSMessage createBaseMessage({
@@ -24,32 +23,4 @@ class MessageFactory {
       ..senderId = senderId
       ..type = type;
   }
-
-  static DHTMessage createFindNodeMessage({
-    required Uint8List targetId,
-    required int numClosestPeers,
-  }) {
-    final findNodeRequest = FindNodeRequest()
-      ..key = targetId
-      ..numClosestPeers = numClosestPeers;
-
-    final now = DateTime.now();
-    final timestamp = Timestamp()
-      ..seconds = Int64(now.millisecondsSinceEpoch ~/ 1000)
-      ..nanos = (now.millisecondsSinceEpoch % 1000) * 1000000;
-
-    final message = DHTMessage()
-      ..messageId = _generateMessageId()
-      ..type = DHTMessage_MessageType.FIND_NODE
-      ..key = targetId
-      ..record = findNodeRequest.writeToBuffer()
-      ..timestamp = timestamp;
-
-    message.closerPeers.addAll([]);
-
-    return message;
-  }
-
-  static String _generateMessageId() =>
-      DateTime.now().millisecondsSinceEpoch.toString();
 }
